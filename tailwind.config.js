9c57e4b5efcb64d600b0ee77e8499e0a2d4afc6f
Angel silva <angelsilvadesantana@gmail.com>
2024-06-14 10:10:31 -0300

    landing page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'dark': '#1a1c2a',
+      }
+    },
+  },
+  plugins: [],
+}
